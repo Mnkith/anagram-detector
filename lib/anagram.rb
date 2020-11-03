2e90ca0b192e513a6730543e1d371b67d71d02dd
Mnkith <munkithabid@gmail.com>
2020-11-02 22:47:34 -0600

    commit tree 744993a0203a05adca28db73d58c18c8d7100881

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+
+class Anagram
+  
+  def initialize(word)
+    @word = word
+  end
+
+  attr_accessor :word
+
+  def match(list)
+    matches = []
+    sorted_word_letters = @word.split('').sort
+    list.each do | item |
+      matches << item if item.split('').sort == sorted_word_letters
+    end
+    matches
+  end
+end
